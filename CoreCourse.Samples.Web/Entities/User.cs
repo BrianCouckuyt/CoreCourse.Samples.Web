2c5e8ac867e4ccb44b57b79d9c91df881a66bd63
Joachim Francois <joachim.francois@howest.be>
2018-05-31 13:50:21 +0200

    Add UserController to the solution Add Entity User

---
+++ CoreCourse.Samples.Web/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCourse.Samples.Web.Entities
+{
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        [ForeignKey("Item")]
+        public int ItemId { get; set; }
+        public Item Item { get; set; }
+
+    }
+}
